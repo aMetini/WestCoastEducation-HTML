9c0db75be395a66b671611dcd5af7c8933660b83
aMetini <71118672+aMetini@users.noreply.github.com>
2021-03-31 14:13:46 +0200

    updated app.js and removed other js files

--- scripts/app.js
+++ scripts/app.js
@@ -1,17 +1,31 @@
-const listCoursesButton = document.querySelector('#listCourses');
+const listCoursesButton = document.querySelectorAll('.listCourses');
+const coursesSection = document.querySelector('#showCourses');
 
 const listCourses = function() {
-    fetch('courses.json')
+    fetch('/data/courses.json')
         .then(response => response.json())
         .then(data => console.log(data))
 }
 
 const listCoursesApi = function() {
-    fetch('http://localhost:3000/courses')
+    fetch('courses.json')
         .then((response) => response.json())
         .then((data) => console.log(data));
 }
 
-listCoursesButton.addEventListener('click', () => {
-    listCoursesApi();
-});
+function getCourses() {
+    fetch('/data/courses.json')
+        .then(response => response.json())
+        .then(data => {
+            coursesSection.innerHTML = '';
+
+            for (let item of data) {
+                coursesSection.insertAdjacentHTML('beforeend',
+                    `<div>
+                ${item.title}
+                </div>`)
+            }
+        })
+}
+
+getCourses();
